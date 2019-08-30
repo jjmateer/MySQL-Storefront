6dba7d6dc1e30b63bf5a738f080ce74b04b07951
John Mateer <jj.mateer@live.com>
2019-08-29 23:53:04 -0700

    Added readProducts function to bamazonManager.js, which displays all of the products in the console.

--- bamazonManager.js
+++ bamazonManager.js
@@ -10,15 +10,44 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-
+    multChoice();
+});
+function multChoice() {
     inquirer.prompt([
         {
             type: 'list',
             name: "multChoice",
             message: "What would you like to do? ",
-            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+            choices: ["View products for sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
         }
     ]).then(answers => {
-        console.log(answers.multChoice)
+            console.log(answers.multChoice)
+        if (answers.multChoice === "View products for sale") {
+            readProducts();
+        }
+        if (answers.multChoice[1]) {
+            // lowInventory();
+        }
     })
-});
+}
+function readProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log("ID: " + res[i].id);
+            console.log("Product Name: " + res[i].product_name);
+            console.log("Department: " + res[i].department_name);
+            console.log("Price: " + res[i].price);
+            console.log("Quantity: " + res[i].stock_quantity)
+            console.log('==========================')
+        }
+    })
+}
+function lowInventory() {
+    // console.log(res[i].stock_quantity)
+    for (var i = 0; i < res.length; i++) {
+        if (res[i].stock_quantity < 3) {
+            console.log(res[i].product_name)
+        }
+    }
+}
