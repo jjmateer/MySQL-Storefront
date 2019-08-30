cfba56302c3563714e1247fca84d0633ec82e7ee
John Mateer <jj.mateer@live.com>
2019-08-29 22:03:16 -0700

    Added mySQL schema that creates a basic table with 10 example items. Added JS file that successfully connects to the 'bamazon' database.

---
+++ bamazonCustomer.js
@@ -0,0 +1,12 @@
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Manila22!",
+  database: "bamazon"
+});
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+  });
