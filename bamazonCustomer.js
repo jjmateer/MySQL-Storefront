a95477d385b1f284dc40e3aa70e89de7845469c5
John Mateer <jj.mateer@live.com>
2019-08-29 22:18:53 -0700

    Added a function that reada all of the rows in the database and displays them in the console

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,12 +1,29 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer")
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Manila22!",
-  database: "bamazon"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Manila22!",
+    database: "bamazon"
 });
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-  });
+});
+readProducts();
+function readProducts() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log("ID: " + res[i].id);
+            console.log("Product Name: " + res[i].product_name);
+            console.log("Department: " + res[i].department_name);
+            console.log("Price: " + res[i].price);
+            console.log("Quantity: " + res[i].stock_quantity)
+            console.log('==========================')
+        }
+        connection.end();
+    });
+}
--- storefrontSchema.sql
+++ storefrontSchema.sql
@@ -1,10 +1,7 @@
 DROP DATABASE IF EXISTS bamazon;
-
 CREATE DATABASE bamazon;
-
 USE bamazon;
-
-CREATE TABLE storefront (
+CREATE TABLE products (
 id INT NOT NULL AUTO_INCREMENT,
 product_name VARCHAR(30) NULL,
 department_name VARCHAR(30) NULL,
@@ -12,23 +9,23 @@ price DECIMAL(5, 2) NULL,
 stock_quantity INTEGER(5)NULL,
 PRIMARY KEY (id)
 );
-INSERT INTO storefront (id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
 VALUES (id, 'T-Shirt', 'Clothing', 20.00, 12);
-INSERT INTO storefront (id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
 VALUES (id, 'Shovel', 'Home & Garden', 25.00, 6);
-INSERT INTO storefront (id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
 VALUES (id, 'Footrest', 'Home & Garden', 45.00, 15);
-INSERT INTO storefront (id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
 VALUES (id, 'Laptop', 'Technology', 800.00, 20);
-INSERT INTO storefront (id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
 VALUES (id, 'Television', 'Technology', 900.00, 10);
-INSERT INTO storefront (id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
 VALUES (id, 'Ribeye steak', 'Food', 25.00, 45);
-INSERT INTO storefront (id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
 VALUES (id, 'Bread', 'Food', 5.00, 30);
-INSERT INTO storefront (id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
 VALUES (id, 'Headphones', 'Technology', 50.00, 20);
-INSERT INTO storefront (id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
 VALUES (id, 'Rug', 'Home & Garden', 80.00, 5);
-INSERT INTO storefront (id, product_name, department_name, price, stock_quantity)
+INSERT INTO products (id, product_name, department_name, price, stock_quantity)
 VALUES (id, 'Hat', 'Clothing', 15.00, 50);
