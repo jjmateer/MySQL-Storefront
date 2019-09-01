8186228a6ea1c418295ad0dd62e007e0b5af7f7e
John Mateer <jj.mateer@live.com>
2019-08-31 21:24:49 -0700

    Added a node package to bamazonSupervisor.js called console table and used it in the salesByDep function to log the products table into the console.

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
-var inquirer = require("inquirer")
+var inquirer = require("inquirer");
+const cTable = require('console.table');
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -29,9 +30,12 @@ function multChoice() {
         }
     })
 }
-
 function salesByDep() {
-
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        const table = cTable.getTable(res)
+        console.log(table);
+    })
 }
 function lowInventory() {
 
